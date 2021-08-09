6b44c3073b94ff4cc84412d4b71d5b4ae5236ce2
Evgen-ios <jack38irk@icloud.com>
2021-08-09 17:30:27 +0800

    Implemented object placement

--- ARKit-Drawing/ViewController.swift
+++ ARKit-Drawing/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     
     private let configuration = ARWorldTrackingConfiguration()
     
+    /// Last node placed by user
+    var lastNode: SCNNode?
+    
+    /// Minimum distance between objects placed when moving
+    let minimumDistance: Float = 0.1
+    
     enum ObjectPlacementMode {
         case freeform, plane, image
     }
@@ -45,8 +51,26 @@ class ViewController: UIViewController {
     
     func  addNode(_ node: SCNNode, to parentNode: SCNNode) {
         
+        // Chech that the object is nottoo close to the previous one
+        if let lastNode = lastNode {
+            let lastPosition = lastNode.position
+            let newPosition = node.position
+            
+            let x = lastPosition.x - newPosition.x
+            let y = lastPosition.y - newPosition.y
+            let z = lastPosition.z - newPosition.z
+            
+            let distanceSquare = x * x + y * y + z * z
+            let minimumDistanceSquare = minimumDistance * minimumDistance
+            
+            guard  minimumDistanceSquare < distanceSquare else { return }
+        }
+        
         // Clone the node for creating separate copies of the object
         let clonedNode = node.clone()
+        
+        // Remember last placed node
+        lastNode = clonedNode
         
         // Remeber object placed for undo
         objectsPlaced.append(clonedNode)
@@ -88,6 +112,15 @@ class ViewController: UIViewController {
         
     }
     
+    func addNodeToImage(_ node: SCNNode, at point: CGPoint) {
+        guard let result = sceneView.hitTest(point, options: [:]).first else { return }
+        guard result.node.name == "image" else { return }
+        node.transform = result.node.worldTransform
+        node.eulerAngles.x = 0
+        addNodeToSceneRoot(node)
+
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showOptions" {
             let optionsViewController = segue.destination as! OptionsContainerViewController
@@ -95,26 +128,38 @@ class ViewController: UIViewController {
         }
     }
     
-    func reloadConfiguration() {
-        configuration.planeDetection = .horizontal
-        sceneView.session.run(configuration)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
+    func process(_ touches: Set<UITouch>) {
         guard let touch = touches.first , let selectedNode = selectedNode else { return }
+        
+        let point = touch.location(in: sceneView)
         
         switch objectMode {
         case .freeform:
             addNodeInFront(selectedNode)
         case .image:
-            break
+            addNodeToImage(selectedNode, at: point)
         case .plane:
-            let point = touch.location(in: sceneView)
             addNode(selectedNode,at: point)
             
         }
+    }
+    
+    func reloadConfiguration() {
+        configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        lastNode = nil
+        process(touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        process(touches)
     }
     
     override func viewDidLoad() {
@@ -182,25 +227,31 @@ extension ViewController: OptionsViewControllerDelegate {
 // MARK: - ARSessionDelegate
 extension ViewController: ARSCNViewDelegate {
     
-    func createFloor(planeAnchor: ARPlaneAnchor) -> SCNNode {
+    func createFloor(with size: CGSize, opacity: CGFloat = 0.35) -> SCNNode {
         
-        // Get estimated plane size
-        let extent = planeAnchor.extent
-        let width = CGFloat(extent.x)
-        let height = CGFloat(extent.z)
-        
-        let plane = SCNPlane(width: width, height: height)
+        let plane = SCNPlane(width: size.width, height: size.height)
         plane.firstMaterial?.diffuse.contents = UIColor.green
         
+        
         let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = opacity
         planeNode.eulerAngles.x -= .pi / 2
-        planeNode.opacity = 0.35
         
         return planeNode
     }
     
+    func nodeAdded(_ node: SCNNode, for anchor: ARImageAnchor) {
+        // Pur a lane at the image
+        let size = anchor.referenceImage.physicalSize
+        let coverNode = createFloor(with: size, opacity: 1)
+        coverNode.name = "image"
+        node.addChildNode(coverNode)
+    }
+    
     func nodeAdded (_ node: SCNNode, for anchor: ARPlaneAnchor) {
-        let planeNode = createFloor(planeAnchor: anchor)
+        let extent = anchor.extent
+        let size = CGSize(width: CGFloat(extent.x), height: CGFloat(extent.z))
+        let planeNode = createFloor(with: size)
         planeNode.isHidden = arePlanesHidden
         
         // Add plane node to the list of plane nodes
@@ -211,6 +262,8 @@ extension ViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         switch anchor {
+        case let imageAnchor as ARImageAnchor:
+            nodeAdded(node, for: imageAnchor)
         case let planeAnchor as ARPlaneAnchor:
             nodeAdded(node, for: planeAnchor)
             break
@@ -221,6 +274,8 @@ extension ViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         switch anchor {
+        case is ARImageAnchor:
+            break
         case let planeAnchor as ARPlaneAnchor:
             updateFloor(for: node, anchor: planeAnchor)
         default:
